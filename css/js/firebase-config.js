61d2d78149a9d9896532e5672303b0491c01a171
UNCHAINEDCASTRO <unchainedcastro@gmail.com>
2025-12-03 03:28:58 +0300

    Create firebase-config.js

---
+++ css/js/firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAZjWEMpF6w_Z8Jtu04LgDKalqaTDTtWYQ",
+    authDomain: "masterpiece-furnitures3.firebaseapp.com",
+    projectId: "masterpiece-furnitures3",
+    storageBucket: "masterpiece-furnitures3.appspot.com",
+    messagingSenderId: "47916634914",
+    appId: "1:47916634914:web:8be8201fddf4a4b10bcb7"
+};
